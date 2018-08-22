22a1392447118ac8becfcd9a5d138f756afd569e
m@rtlin2cz <martlin2cz@gmail.com>
2018-08-22 17:23:32 +0200

    Created initial nodejs structure
     - created app.js, generator.js with simple hello world content

---
+++ app.js
@@ -0,0 +1,3 @@
+var generator = require('./js/generator.js');
+
+console.log(generator.generate(null, null)); //TODO params
---
+++ js/generator.js
@@ -0,0 +1,14 @@
+/**
+	* The generator of "Jde chleba a potká ..."
+	* m@rtlin, 22.8.2018
+	*
+	*/
+
+exports.generate = function (pastries, spreads) {
+	return generate(pastries, spreads);
+};
+
+
+function generate(pastries, spreads) {
+	return "Jde chleba a potká ..."; //TODO here
+}
